b44f32e1cbadc4b7f63ac04a13dcd07a93880c40
Matias Bovina <matiasbovina@Mac-mini-de-Matias.local>
2023-05-13 11:22:39 +0200

    added-impl-to-contact-controller

--- src/main/java/com/atrezzo/manager/presentation/controller/impl/ContactControllerImpl.java
+++ src/main/java/com/atrezzo/manager/presentation/controller/impl/ContactControllerImpl.java
@@ -6,10 +6,14 @@ import com.atrezzo.manager.domain.model.Contact;
 import com.atrezzo.manager.presentation.controller.ContactController;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("api/contacts")
@@ -35,31 +39,63 @@ public class ContactControllerImpl implements ContactController {
 
     @Override
     public ResponseEntity<?> findContactByNameOrCompany(String firstName, String lastName, String companyName) {
-        return null;
+        try {
+            Contact contact = contactService.findContact(firstName, lastName, companyName);
+            return ResponseEntity.ok(modelMapper.map(contact, ContactDTO.class));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while trying to find contact");
+        }
     }
 
     @Override
     public ResponseEntity<?> findContactById(Long id) {
-        return null;
+        try {
+            Contact contact = contactService.findContactById(id);
+            return ResponseEntity.ok(modelMapper.map(contact, ContactDTO.class));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while trying to find contact");
+        }
     }
 
     @Override
     public ResponseEntity<?> findAllContactByPage(int pageNumber, int pageSize) {
-        return null;
+        try {
+            Page<Contact> contactsPage = contactService.findAllContacts(pageNumber, pageSize);
+            Page<ContactDTO> dtoPage = contactsPage.map(contact -> modelMapper.map(contact, ContactDTO.class));
+            return ResponseEntity.ok(dtoPage);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while trying to find contacts");
+        }
     }
 
     @Override
     public ResponseEntity<?> findContactsByCompanyName(String companyName) {
-        return null;
+        try {
+            List<Contact> contacts = contactService.findAllContactsByCompanyName(companyName);
+            List<ContactDTO> dtoList = contacts.stream().map(contact -> modelMapper.map(contact, ContactDTO.class)).collect(Collectors.toList());
+            return ResponseEntity.ok(dtoList);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while trying to find contacts");
+        }
     }
 
     @Override
     public ResponseEntity<?> updateContact(ContactDTO contactDTO) {
-        return null;
+        try {
+            Contact updatedContact = contactService.updateContact(modelMapper.map(contactDTO, Contact.class));
+            return ResponseEntity.ok(modelMapper.map(updatedContact, ContactDTO.class));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while trying to update contact");
+        }
     }
 
     @Override
     public ResponseEntity<?> deleteContactById(Long id) {
-        return null;
+        try {
+            contactService.deleteContactById(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while trying to delete contact");
+        }
     }
 }
